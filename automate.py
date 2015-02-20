7ae1e9ae78ad1ce276c6e60d990717f2e55fc4fd
Deep Sukhwani <deepsukhwani@gmail.com>
2015-02-20 18:50:38 +0530

    Code Refactored! Moved all variables to 'constants.py' and added two line code to automate.py to deal with Confirm dialog in case of price drop after clicking Book button on search results page

--- automate.py
+++ automate.py
@@ -7,26 +7,31 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 
+# Custom variables Constants are in constants.py
+from constants import *
+
 # Get today's date in "day", "month" and "year"
 date = datetime.datetime.now()
 day = date.day
 month = date.month
 year = date.year
-month_dict = {'January': 1, 'February': 2, 'March': 3, 'April': 4, 'May': 5, 'June': 6, 'July': 7, 'August': 8, 'September': 9, 'October': 10, 'November': 11, 'December': 12}
+# month_dict = {'January': 1, 'February': 2, 'March': 3, 'April': 4, 'May': 5, 'June': 6, 'July': 7, 'August': 8, 'September': 9, 'October': 10, 'November': 11, 'December': 12}
 
-URL = "http://www.makemytrip.com"
-RADIO_BUTTON_ID = 'round_trip_button1'
-MAKE_MY_TRIP = 'MakeMyTrip'
-FROM_INPUT_ID = 'from_typeahead1'
-TO_INPUT_ID = 'to_typeahead1'
-DEPART_DATE_ID = 'start_date_sec'
-RETURN_DATE_ID = 'return_date_sec'
-FROM_CITY = 'BLR'
-TO_CITY = 'DEL'
-DATEPICKER_MONTH_XPATH = '//*[@id="ui-datepicker-div"]/div[2]/div/div/span[1]'
-DATEPICKER_YEAR_XPATH = '//*[@id="ui-datepicker-div"]/div[2]/div/div/span[2]'
-SEARCH_BUTTON_ID = "flights_submit"
-MODAL_DIALOG_CLASS = 'modal-dialog'
+# URL = "http://www.makemytrip.com"
+# RADIO_BUTTON_ID = 'round_trip_button1'
+# MAKE_MY_TRIP = 'MakeMyTrip'
+# FROM_INPUT_ID = 'from_typeahead1'
+# TO_INPUT_ID = 'to_typeahead1'
+# DEPART_DATE_ID = 'start_date_sec'
+# RETURN_DATE_ID = 'return_date_sec'
+# FROM_CITY = 'BLR'
+# TO_CITY = 'DEL'
+# DATEPICKER_MONTH_XPATH ='//*[@id="ui-datepicker-div"]/div[2]/div/div/span[1]'
+# DATEPICKER_YEAR_XPATH = '//*[@id="ui-datepicker-div"]/div[2]/div/div/span[2]'
+# SEARCH_BUTTON_ID = "flights_submit"
+# MODAL_DIALOG_CLASS = 'modal-dialog'
+# BOOK_BUTTON_FOOTER_CSS = 'span.clearfix > a'
+# CONFIRM_MODAL_DIALOG_ID = 'fcModalBody'
 
 class mmtAutomation(unittest.TestCase):
 
@@ -36,10 +41,11 @@ class mmtAutomation(unittest.TestCase):
         """
         firefox_profile = webdriver.FirefoxProfile()
         self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(10)
 
-    def tearDown(self):
-        driver = self.driver
-        driver.close()
+    # def tearDown(self):
+        # driver = self.driver
+        # driver.close()
 
     # def test_001_load_webpage(self):
         
@@ -84,6 +90,18 @@ class mmtAutomation(unittest.TestCase):
             
             if modal_dialog:
                 driver.find_element_by_link_text('Search Flight').click()
+            
+            # time.sleep(3)
+            
+            book_button_footer = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, BOOK_BUTTON_FOOTER_CSS)))
+            
+            book_button_footer.click()
+            
+            try:
+                confirm_modal_dialog = driver.find_element_by_id(CONFIRM_MODAL_DIALOG_ID)
+                driver.find_element_by_link_text('Continue to Book').click()
+            except:
+                pass
             
             # MONTH = driver.find_element_by_xpath(DATEPICKER_MONTH_XPATH)
             # YEAR = driver.find_element_by_xpath(DATEPICKER_YEAR_XPATH)
